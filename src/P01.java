25e3a25d7699cf6269eeddaf8c30fc82ade130c6
rananurulker <rananurulker@gmail.com>
2024-07-01 10:29:06 +0300

    v 2.0

--- src/P01.java
+++ src/P01.java
@@ -1,5 +1,6 @@
-public class GitHub {
+public class P01 {
     public static void main(String[] args) {
         System.out.println("Merhaba Git Hub");
+        System.out.println("ikinci dosya eklendi.");
     }
 }
---
+++ src/P02.java
@@ -0,0 +1,5 @@
+public class P02 {
+    public static void main(String[] args) {
+        System.out.println("Ikinci dosya eklendi.");
+    }
+}
