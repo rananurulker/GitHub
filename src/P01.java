85442065121dee944de72eaa55dc53cdfeafdfa0
rananurulker <rananurulker@gmail.com>
2024-07-01 10:47:01 +0300

    v 3.2

--- src/P01.java
+++ src/P01.java
@@ -2,5 +2,6 @@ public class P01 {
     public static void main(String[] args) {
         System.out.println("Merhaba Git Hub");
         System.out.println("ikinci dosya eklendi.");
+        System.out.println("versiyon guncelleme");
     }
 }
--- src/P03.java
+++ src/P03.java
@@ -1,6 +1,7 @@
 public class P03 {
     public static void main(String[] args) {
         System.out.println("Yeni modul ekleniyor");
+        System.out.println("deneme");
 
     }
 }
