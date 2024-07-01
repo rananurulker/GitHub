1f60f42c9b43fe8a7d8125cb7aad7e77d738a3bf
rananurulker <rananurulker@gmail.com>
2024-07-01 10:35:37 +0300

    v 2.1

--- src/P02.java
+++ src/P02.java
@@ -1,5 +1,10 @@
 public class P02 {
     public static void main(String[] args) {
         System.out.println("Ikinci dosya eklendi.");
+        System.out.println("Denem versiyou");
+        int a=120;
+        int b=30;
+
+        System.out.println("toplam= "+ (a+b));
     }
 }
